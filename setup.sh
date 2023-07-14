147e34e2cc87c4e3d1b469586b333f7f82b2060a
Nusapay <nusapay@o-o.local>
2023-07-14 12:37:10 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 if [ -z  "$*" ];
-    dir = "test"
 then
-    dir = "$1"
+    dir=$("test")
+else
+    dir="$1"
 fi
-if [ -d "./$dir" ]; then
+
+if [ -d "./$dir" ];
+then
   echo "./$dir already exists."
 fi
 git clone https://github.com/godamri/test-framework.git $dir
