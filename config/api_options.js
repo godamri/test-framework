0e48808a0d96b7e069d6ae0a443ebbe42c6262c3
Rian <10650472+godamri@users.noreply.github.com>
2023-10-04 13:05:48 +0700

    Add files via upload

---
+++ config/api_options.js
@@ -0,0 +1,10 @@
+module.exports = {
+    driverOptions: {},
+    url : 'https://dummyapi.online',
+    metaData: {
+        "App Version": "1.0-DEV",
+        "Test Environment": "Dev",
+        "Platform": "MacOS",
+        "Executed": "Local"
+    },
+}
