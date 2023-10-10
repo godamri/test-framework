3d2ea498b3e3237e2264e024a7557be766dac7cb
Rian <10650472+godamri@users.noreply.github.com>
2023-10-10 08:00:45 +0000

    Update before.js

--- samples/api/features/hooks/before.js
+++ samples/api/features/hooks/before.js
@@ -1,38 +1,16 @@
 const { BeforeAll, Before, setDefaultTimeout, setWorldConstructor } = require('@cucumber/cucumber')
 const { customWorld } = require("../../support/support.js");
-const { remote } = require('webdriverio');
-const {driverOptions, url} = require("../../config/driver_options.js");
+const {url} = require("../../config/driver_options.js");
 
 setDefaultTimeout(60 * 10000);
 setWorldConstructor(customWorld);
-let driver, screen;
 BeforeAll(async function () {
-    // driver = await remote(driverOptions);
-    // await driver.url(url);
-    // await driver.pause(3000);
+    customWorld.state = {
+        api: {
+            base_url: url
+        }
+    };
 });
-Before(async function () {
-    // this.driver = driver;
-    // this.screen = screen;
-});
+Before(async function () {});
 
-Before({ tags: "@Login" }, async function () {
-    // try {
-    //     // clearing login form
-    //     console.log('');
-    //     console.log('Preparing login screen...');
-    //     const loginScreenInputEmail = await this.driver.$("#email");
-    //     const e = await loginScreenInputEmail.waitForDisplayed({ timeout: 3000 });
-    //     const loginScreenInputPassword = await this.driver.$("id=password");
-    //     const p = await loginScreenInputPassword.waitForDisplayed({ timeout: 3000 });
-    //     if (e) {
-    //         await loginScreenInputEmail.setValue('');
-    //     }
-    //     if (p) {
-    //         await loginScreenInputPassword.setValue('');
-    //     }
-    //     console.log('Login screen ready');
-    // } catch (error) {
-    //     console.log('Login screen not ready!');
-    // }
-});
+Before({ tags: "@Login" }, async function () {});
