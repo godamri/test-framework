eedaa1da1f221a082c95f5ba422dcb5bde31ece0
Rian <10650472+godamri@users.noreply.github.com>
2023-10-10 08:45:41 +0000

    Update search.steps.js

--- samples/browser/features/steps_definition/search.steps.js
+++ samples/browser/features/steps_definition/search.steps.js
@@ -1,17 +1,18 @@
-const { Given, When } = require('@cucumber/cucumber');
+const { Given, When, Then } = require('@cucumber/cucumber');
 const { should } = require('chai');
+const { capture } = require('../../support/support');
 
 Given('User arrives in google search homepage', async function () {
-    const inputSearch = await this.driver.$("input[type=search]");
+    const inputSearch = await this.driver.$("textarea[type=search]");
     await inputSearch.waitForDisplayed({ timeout: 5000 });
     await should().exist(inputSearch.elementId, 'inputEmail field does not exist');
 });
 
 When('User search word {}', async function (keyword) {
-    const inputSearch = await this.driver.$("input[type=search]");
+    const inputSearch = await this.driver.$("textarea[type=search]");
     const e = await inputSearch.waitForDisplayed({ timeout: 5000 });
     if (e) {
-        await loginScreenInputEmail.setValue(keyword);
+        await inputSearch.setValue(keyword);
     }
 });
 
@@ -21,4 +22,5 @@ Then('User click search button', async function () {
     if (p) {
         await btnSearch.click();
     }
+    await capture(this);
 });
