db0949d91203cd8c6f0dc367fef751af2d8de7c0
Rian <10650472+godamri@users.noreply.github.com>
2023-10-10 08:38:36 +0000

    Update before.js

--- samples/browser/features/hooks/before.js
+++ samples/browser/features/hooks/before.js
@@ -21,18 +21,18 @@ Before({ tags: "@Login" }, async function () {
         // clearing login form
         console.log('');
         console.log('Preparing login screen...');
-        const loginScreenInputEmail = await this.driver.$("#email");
-        const e = await loginScreenInputEmail.waitForDisplayed({ timeout: 3000 });
-        const loginScreenInputPassword = await this.driver.$("id=password");
-        const p = await loginScreenInputPassword.waitForDisplayed({ timeout: 3000 });
-        if (e) {
-            await loginScreenInputEmail.setValue('');
-        }
-        if (p) {
-            await loginScreenInputPassword.setValue('');
-        }
-        console.log('Login screen ready');
+        // const loginScreenInputEmail = await this.driver.$("#email");
+        // const e = await loginScreenInputEmail.waitForDisplayed({ timeout: 3000 });
+        // const loginScreenInputPassword = await this.driver.$("id=password");
+        // const p = await loginScreenInputPassword.waitForDisplayed({ timeout: 3000 });
+        // if (e) {
+        //     await loginScreenInputEmail.setValue('');
+        // }
+        // if (p) {
+        //     await loginScreenInputPassword.setValue('');
+        // }
+        console.log('home screen ready');
     } catch (error) {
-        console.log('Login screen not ready!');
+        console.log('home screen not ready!');
     }
 });
